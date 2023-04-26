303433d97726268361f91d481b9aec068bc1bc37
BrandonHC <brandonhgcampos@gmail.com>
2023-04-25 20:34:13 -0500

    Progress #2

--- GPDesign.c
+++ GPDesign.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
-#define n 10
+#define n 50
+#define MAXROUTES 10
+
+typedef struct {
+	int seats[6][40];
+} flight_t;
 
 typedef struct {
 	//enum fl_t;
 	int id;
-} customer_t; 
+} customer_t;
 
 typedef struct {
 	/* write rest of code here */
+	int id;
 } staff_t;
+
+ void printflSeats(flight_t* flight, int route) {
+	for(int i = 0; i < 6; ++i) {
+		printf("[ ] ");
+			for(int j = 0; j < 40; ++j)
+				printf("[ ] ");
+		printf("\n");
+
+		if(i == 2)
+			printf("\n");
+	}
+}
 
 void accountCreate (customer_t* client, int *registry) {
 	client[*registry].id = *registry + 1; //Implements ID
@@ -35,8 +53,9 @@ void accountAuthenticate(customer_t* client) {
 	return;
 }
 
-void customerInterface(customer_t* client, int *registry) {
+void customerInterface(customer_t* client, flight_t* flight, int *registry) {
 	int uAccount = 0;
+	int route;
 
 	while(uAccount != 3) {
 		printf("\nAre you a new or returning customer?\n");
@@ -55,22 +74,46 @@ void customerInterface(customer_t* client, int *registry) {
 				accountCreate(client, registry);
 		} else if (uAccount == 2)
 			accountAuthenticate(client);
+
+		printf("\nThere are 2 flights available at this moment.\n");
+		printf("[1] Chicago\t[2] Austin\nSelect one of the options above: \n");
+
+		do {
+			scanf("%d", route);
+			if(!(route == 1 || route == 2))
+				printf("Error, please select a valid option and try again: ");
+		} while (!(route == 1 || route == 2));
+
+		printflSeats(flight, route);
 	}
 	
 	return;
 }
 
 void companyInterface() {
+    
+}
 
+void initializeFlights(flight_t* flights, int flightNum) {
+    for(int i = 0; i < 6; ++i) {
+            for(int j = 0; j < 40; ++j)
+                flights[flightNum].seats[i][j] = 0;
+    }
 }
 
 int main() {
 	int userOption = 0;
 	customer_t client[n] = {0};
+	staff_t staff[n] = {0};
+	flight_t flight[MAXROUTES];
 	int registry = 0;
+	int staffReg = 0;
 
+    for(int i = 0; i < MAXROUTES; ++i)
+        initializeFlights(flight, i);
+    
 	while(userOption != 3) {
-		printf("\nWelcome to Alaska International Airways\n");
+		printf("Welcome to Milk Airlines\n");
 		printf("[1] Customer\t[2] Staff\n[3] Exit Program\n");
 		printf("Select one of the displayed options above: ");
 
@@ -82,7 +125,7 @@ int main() {
 
 		switch(userOption) {
 			case 1:
-				customerInterface(client, &registry);
+				customerInterface(client, flight, &registry);
 				break;
 			case 2:
 				companyInterface();
@@ -91,7 +134,7 @@ int main() {
 
 		/* for(int i = 0; i < n; ++i) {
 			printf("ARRAY INDEX: %d, CLIENT ID: %d\n", i, client[i].id);
-		} FOR DEBUGGING PURPOSES - LISTS DATA WITHIN CLIENT STRUCT ARRAY AND CORRESPONDING ID*/ 
+		} //FOR DEBUGGING PURPOSES - LISTS DATA WITHIN CLIENT STRUCT ARRAY AND CORRESPONDING ID */
 	}
 
 	return 0;
